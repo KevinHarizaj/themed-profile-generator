cb1174cf2077841cb1169ee2b41ebd32d996b9d3
fhs42726 <lheiglauer.mmt-m2021@fh-salzburg.ac.at>
2022-05-22 21:16:35 +0200

    Setup cli and add user input with prompts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,68 @@
-import { randomName } from './random-name.js'
-export { randomName }
+//import { randomName } from './random-name'
+//export { randomName }
+
+const chalk = require('chalk') // string styling
+const clear = require('clear') // clearing the window
+const figlet = require('figlet') // ASCII strings
+//const path = require('path') // path module
+const program = require('commander') // command-line interface prompter
+const cmdPrompt = require('./inquire')
+const fs = require('fs')
+
+clear()
+console.log(
+    chalk.blue(
+        figlet.textSync('Themed Profile Generator', {
+            font: 'Big',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 80,
+            whitespaceBreak: true
+        })
+    )
+)
+
+async function run(): Promise<void> {
+    const theme = await cmdPrompt.askTheme()
+    console.log('Your theme: ', theme.theme)
+    const numberOfProfiles = await cmdPrompt.askNumberOfProfiles()
+    console.log('Number of profiles: ', numberOfProfiles.number)
+
+    // generate profiles here
+    // ...
+
+    // dummy profiles
+    const profiles = [
+        {
+            name: 'Tom',
+            age: 32,
+            job: 'Photographer'
+        },
+        {
+            name: 'Elsa',
+            age: 23,
+            job: 'Princess'
+        }
+    ]
+
+    const jsonString = JSON.stringify(profiles)
+    fs.writeFile('./tpg.json', jsonString, (err: any) => {
+        if (err) {
+            console.log('There was an error writing the file', err)
+        } else {
+            console.log(
+                `Thank you! ${numberOfProfiles.number} ${theme.theme} profiles were generated and successfully written to the file tpg.json`
+            )
+        }
+    })
+}
+
+// const listThemes = () => { return }
+
+// program
+//     .command('themes')
+//     .description('List all available themes')
+//     .action(listThemes)
+// program: list themes, generate profiles -arg -arg ???
+
+run()
---
+++ src/inquire.ts
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer')
+
+module.exports = {
+    askUsername: () => {
+        const questions: object[] = [
+            {
+                name: 'username',
+                type: 'input',
+                message: 'Enter your username to get your report:',
+                validate: function (value: any) {
+                    if (value.length) {
+                        return true
+                    } else {
+                        return 'Please enter a username.'
+                    }
+                }
+            }
+        ]
+        return inquirer.prompt(questions)
+    },
+    askTheme: () => {
+        const questions: object[] = [
+            {
+                name: 'theme',
+                type: 'list',
+                message: 'Which theme do you want? (Use arrow keys)',
+                choices: ['Middle Ages', 'Sci-Fi Future']
+            }
+        ]
+        return inquirer.prompt(questions)
+    },
+    askNumberOfProfiles: () => {
+        const maximumProfiles = 5
+        const questions: object[] = [
+            {
+                name: 'number',
+                type: 'number',
+                message:
+                    'How many profiles do you want to be generated? Default:',
+                default: 3,
+                validate: function (value: any) {
+                    const isNumber = !isNaN(parseInt(value))
+                    const isInRange =
+                        parseInt(value) > 0 && parseInt(value) < maximumProfiles
+                    if (isNumber && isInRange) {
+                        return true
+                    } else {
+                        return `Please enter a number between 0 and ${maximumProfiles}`
+                    }
+                }
+            }
+        ]
+        return inquirer.prompt(questions)
+    }
+}
